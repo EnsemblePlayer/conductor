fd2cbe248488e6d4b6ced4cef467611910d21999
Anthony <ambauer@utexas.edu>
2015-08-28 18:40:25 -0500

    i think this is it

--- public/index.php
+++ public/index.php
@@ -515,8 +515,8 @@ $app->delete('/playlists/:playlist/permissions/:user', function ($playlistId,$us
 *****************************************************************************************************************************************************************************************/
 
 $app->put('/rooms/:id', function ($roomId) use ($app, $config, $m) { //update room data
-	$roomName = $app->request->post('roomName');
-	$password = $app->request->post('password');
+	$roomName = $app->request->put('roomName');
+	$password = $app->request->put('password');
 	$m->query("UPDATE `roomData` SET `name`='$roomName' WHERE `roomId`='$roomId'")or die($m->error);
 	$m->query("UPDATE `roomData` SET `password`='$password' WHERE `roomId`='$roomId'")or die($m->error);
 	$app->response->setStatus(200);
