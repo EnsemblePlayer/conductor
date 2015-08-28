0fd008674f619cd48100567e8c7a2edb93738e83
Anthony <ambauer@utexas.edu>
2015-08-28 18:59:12 -0500

    More debugging

--- public/index.php
+++ public/index.php
@@ -519,7 +519,6 @@ $app->put('/rooms/:id', function ($roomId) use ($app, $config, $m) { //update ro
 	$password = $app->request->put('password');
 	echo $app->request->body();
 	echo $app->request->put();
-	echo $app->request->post();
 	$m->query("UPDATE `roomData` SET `name`='$roomName' WHERE `roomId`='$roomId'")or die($m->error);
 	$m->query("UPDATE `roomData` SET `password`='$password' WHERE `roomId`='$roomId'")or die($m->error);
 	$app->response->setStatus(200);
