5480d4ee58dab33bf9d6ab7fad5a9147f718328b
Anthony <ambauer@utexas.edu>
2015-08-28 19:00:11 -0500

    one last try

--- public/index.php
+++ public/index.php
@@ -517,7 +517,6 @@ $app->delete('/playlists/:playlist/permissions/:user', function ($playlistId,$us
 $app->put('/rooms/:id', function ($roomId) use ($app, $config, $m) { //update room data
 	$roomName = $app->request->put('name');
 	$password = $app->request->put('password');
-	echo $app->request->body();
 	echo $app->request->put();
 	$m->query("UPDATE `roomData` SET `name`='$roomName' WHERE `roomId`='$roomId'")or die($m->error);
 	$m->query("UPDATE `roomData` SET `password`='$password' WHERE `roomId`='$roomId'")or die($m->error);
